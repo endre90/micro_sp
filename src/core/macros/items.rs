00c9fde24288a4766c3440fa55b484bd47593cc0
endre90 <endre90@gmail.com>
2020-10-30 22:09:32 +0100

    macros and mtests for enums

--- src/core/macros/items.rs
+++ src/core/macros/items.rs
@@ -1,6 +1,6 @@
 use super::*;
 
-/// a new boolean variable of command kind
+/// make a new boolean variable of command kind
 #[macro_export]
 macro_rules! bool_c {
     ($name:expr) => {
@@ -25,7 +25,7 @@ macro_rules! bool_c {
     };
 }
 
-/// a new boolean variable of measured kind
+/// make a new boolean variable of measured kind
 #[macro_export]
 macro_rules! bool_m {
     ($name:expr) => {
@@ -50,7 +50,7 @@ macro_rules! bool_m {
     };
 }
 
-/// a new boolean variable of estimated kind
+/// make a new boolean variable of estimated kind
 #[macro_export]
 macro_rules! bool_e {
     ($name:expr) => {
@@ -75,7 +75,7 @@ macro_rules! bool_e {
     };
 }
 
-/// assign a value to a variable
+/// assign a value to a boolean variable
 #[macro_export]
 macro_rules! bool_assign {
     ($var:expr, $val:expr) => {
@@ -86,7 +86,7 @@ macro_rules! bool_assign {
     };
 }
 
-/// make a new variable and assign a value to it
+/// make a new boolean variable of command kind and assign a value to it
 #[macro_export]
 macro_rules! new_bool_assign_c {
     ($name:expr, $val:expr) => {
@@ -133,7 +133,7 @@ macro_rules! new_bool_assign_c {
     };
 }
 
-/// make a new variable and assign a value to it
+/// make a new boolean variable of measured kind and assign a value to it
 #[macro_export]
 macro_rules! new_bool_assign_m {
     ($name:expr, $val:expr) => {
@@ -180,7 +180,7 @@ macro_rules! new_bool_assign_m {
     };
 }
 
-/// make a new variable and assign a value to it
+/// make a new boolean variable of estimated kind and assign a value to it
 #[macro_export]
 macro_rules! new_bool_assign_e {
     ($name:expr, $val:expr) => {
@@ -222,6 +222,278 @@ macro_rules! new_bool_assign_e {
                 Some(&Kind::Estimated),
             ),
             &$val.to_spvalue(),
+            Some(&$life),
+        )
+    };
+}
+
+/// make a new enumeration variable of command kind
+#[macro_export]
+macro_rules! enum_c {
+    ($name:expr, $r#type:expr, $domain:expr) => {
+        Variable::new(
+            $name,
+            &SPValueType::String,
+            &$domain
+                .iter()
+                .map(|x| String::from(x.to_owned()).to_spvalue())
+                .collect(),
+            None,
+            Some(&String::from($r#type)),
+            Some(&Kind::Command),
+        )
+    };
+    ($name:expr, $r#type:expr, $domain:expr, $param:expr) => {
+        Variable::new(
+            $name,
+            &SPValueType::String,
+            &$domain
+                .iter()
+                .map(|x| String::from(x.to_owned()).to_spvalue())
+                .collect(),
+                Some(&Parameter::new(&$param, &true)),
+            Some(&String::from($r#type)),
+            Some(&Kind::Command),
+        )
+    };
+}
+
+/// make a new enumeration variable of measured kind
+#[macro_export]
+macro_rules! enum_m {
+    ($name:expr, $r#type:expr, $domain:expr) => {
+        Variable::new(
+            $name,
+            &SPValueType::String,
+            &$domain
+                .iter()
+                .map(|x| String::from(x.to_owned()).to_spvalue())
+                .collect(),
+            None,
+            Some(&String::from($r#type)),
+            Some(&Kind::Measured),
+        )
+    };
+    ($name:expr, $r#type:expr, $domain:expr, $param:expr) => {
+        Variable::new(
+            $name,
+            &SPValueType::String,
+            &$domain
+                .iter()
+                .map(|x| String::from(x.to_owned()).to_spvalue())
+                .collect(),
+                Some(&Parameter::new(&$param, &true)),
+            Some(&String::from($r#type)),
+            Some(&Kind::Measured),
+        )
+    };
+}
+
+/// make a new enumeration variable of estimated kind
+#[macro_export]
+macro_rules! enum_e {
+    ($name:expr, $r#type:expr, $domain:expr) => {
+        Variable::new(
+            $name,
+            &SPValueType::String,
+            &$domain
+                .iter()
+                .map(|x| String::from(x.to_owned()).to_spvalue())
+                .collect(),
+            None,
+            Some(&String::from($r#type)),
+            Some(&Kind::Estimated),
+        )
+    };
+    ($name:expr, $r#type:expr, $domain:expr, $param:expr) => {
+        Variable::new(
+            $name,
+            &SPValueType::String,
+            &$domain
+                .iter()
+                .map(|x| String::from(x.to_owned()).to_spvalue())
+                .collect(),
+                Some(&Parameter::new(&$param, &true)),
+            Some(&String::from($r#type)),
+            Some(&Kind::Estimated),
+        )
+    };
+}
+
+/// assign a value to an enumeration variable
+#[macro_export]
+macro_rules! enum_assign {
+    ($var:expr, $val:expr) => {
+        Assignment::new(&$var, &String::from($val).to_spvalue(), None)
+    };
+    ($var:expr, $val:expr, $life:expr) => {
+        Assignment::new(&$var, &String::from($val).to_spvalue(), Some(&$life))
+    };
+}
+
+/// make a new enumeration variable of command kind and assign a value to it
+#[macro_export]
+macro_rules! new_enum_assign_c {
+    ($name:expr, $r#type:expr, $domain:expr, $val:expr) => {
+        Assignment::new(
+            &Variable::new(
+                $name,
+                &SPValueType::String,
+                &$domain
+                    .iter()
+                    .map(|x| String::from(x.to_owned()).to_spvalue())
+                    .collect(),
+                None,
+                Some(&String::from($r#type)),
+                Some(&Kind::Command),
+            ),
+            &String::from($val.to_owned()).to_spvalue(),
+            None,
+        )
+    };
+    ($name:expr, $r#type:expr, $domain:expr, $val:expr, $param:expr) => {
+        Assignment::new(
+            &Variable::new(
+                $name,
+                &SPValueType::String,
+                &$domain
+                    .iter()
+                    .map(|x| String::from(x.to_owned()).to_spvalue())
+                    .collect(),
+                Some(&Parameter::new(&$param, &true)),
+                Some(&String::from($r#type)),
+                Some(&Kind::Command),
+            ),
+            &String::from($val.to_owned()).to_spvalue(),
+            None,
+        )
+    };
+    ($name:expr, $r#type:expr, $domain:expr, $val:expr, $param:expr, $life:expr) => {
+        Assignment::new(
+            &Variable::new(
+                $name,
+                &SPValueType::String,
+                &$domain
+                    .iter()
+                    .map(|x| String::from(x.to_owned()).to_spvalue())
+                    .collect(),
+                Some(&Parameter::new(&$param, &true)),
+                Some(&String::from($r#type)),
+                Some(&Kind::Command),
+            ),
+            &String::from($val.to_owned()).to_spvalue(),
+            Some(&$life),
+        )
+    };
+}
+
+/// make a new enumeration variable of measured kind and assign a value to it
+#[macro_export]
+macro_rules! new_enum_assign_m {
+    ($name:expr, $r#type:expr, $domain:expr, $val:expr) => {
+        Assignment::new(
+            &Variable::new(
+                $name,
+                &SPValueType::String,
+                &$domain
+                    .iter()
+                    .map(|x| String::from(x.to_owned()).to_spvalue())
+                    .collect(),
+                None,
+                Some(&String::from($r#type)),
+                Some(&Kind::Measured),
+            ),
+            &String::from($val.to_owned()).to_spvalue(),
+            None,
+        )
+    };
+    ($name:expr, $r#type:expr, $domain:expr, $val:expr, $param:expr) => {
+        Assignment::new(
+            &Variable::new(
+                $name,
+                &SPValueType::String,
+                &$domain
+                    .iter()
+                    .map(|x| String::from(x.to_owned()).to_spvalue())
+                    .collect(),
+                Some(&Parameter::new(&$param, &true)),
+                Some(&String::from($r#type)),
+                Some(&Kind::Measured),
+            ),
+            &String::from($val.to_owned()).to_spvalue(),
+            None,
+        )
+    };
+    ($name:expr, $r#type:expr, $domain:expr, $val:expr, $param:expr, $life:expr) => {
+        Assignment::new(
+            &Variable::new(
+                $name,
+                &SPValueType::String,
+                &$domain
+                    .iter()
+                    .map(|x| String::from(x.to_owned()).to_spvalue())
+                    .collect(),
+                Some(&Parameter::new(&$param, &true)),
+                Some(&String::from($r#type)),
+                Some(&Kind::Measured),
+            ),
+            &String::from($val.to_owned()).to_spvalue(),
+            Some(&$life),
+        )
+    };
+}
+
+/// make a new enumeration variable of estimated kind and assign a value to it
+#[macro_export]
+macro_rules! new_enum_assign_e {
+    ($name:expr, $r#type:expr, $domain:expr, $val:expr) => {
+        Assignment::new(
+            &Variable::new(
+                $name,
+                &SPValueType::String,
+                &$domain
+                    .iter()
+                    .map(|x| String::from(x.to_owned()).to_spvalue())
+                    .collect(),
+                None,
+                Some(&String::from($r#type)),
+                Some(&Kind::Estimated),
+            ),
+            &String::from($val.to_owned()).to_spvalue(),
+            None,
+        )
+    };
+    ($name:expr, $r#type:expr, $domain:expr, $val:expr, $param:expr) => {
+        Assignment::new(
+            &Variable::new(
+                $name,
+                &SPValueType::String,
+                &$domain
+                    .iter()
+                    .map(|x| String::from(x.to_owned()).to_spvalue())
+                    .collect(),
+                Some(&Parameter::new(&$param, &true)),
+                Some(&String::from($r#type)),
+                Some(&Kind::Estimated),
+            ),
+            &String::from($val.to_owned()).to_spvalue(),
+            None,
+        )
+    };
+    ($name:expr, $r#type:expr, $domain:expr, $val:expr, $param:expr, $life:expr) => {
+        Assignment::new(
+            &Variable::new(
+                $name,
+                &SPValueType::String,
+                &$domain
+                    .iter()
+                    .map(|x| String::from(x.to_owned()).to_spvalue())
+                    .collect(),
+                Some(&Parameter::new(&$param, &true)),
+                Some(&String::from($r#type)),
+                Some(&Kind::Estimated),
+            ),
+            &String::from($val.to_owned()).to_spvalue(),
             Some(&$life),
         )
     };
--- src/core/macros/tests/items.rs
+++ src/core/macros/tests/items.rs
@@ -12,6 +12,26 @@ fn is_bool_fruit(fruit: &str, param: &str, kind: &Kind) -> Variable {
     }
 }
 
+fn is_enum_fruit(
+    fruit: &str,
+    r#type: &str,
+    domain: &Vec<&str>,
+    param: &str,
+    kind: &Kind,
+) -> Variable {
+    Variable {
+        name: fruit.to_owned(),
+        value_type: SPValueType::String,
+        domain: domain
+            .iter()
+            .map(|x| String::from(x.to_owned()).to_spvalue())
+            .collect(),
+        param: Parameter::new(&param, &true),
+        r#type: r#type.to_owned(),
+        kind: kind.to_owned(),
+    }
+}
+
 fn is_bool_fruit_true(fruit: &str, param: &str, kind: &Kind, life: u64) -> Assignment {
     Assignment {
         var: Variable {
@@ -23,6 +43,32 @@ fn is_bool_fruit_true(fruit: &str, param: &str, kind: &Kind, life: u64) -> Assig
             kind: kind.to_owned(),
         },
         val: SPValue::Bool(true),
+        lifetime: Duration::from_millis(life),
+    }
+}
+
+fn is_enum_fruit_something(
+    fruit: &str,
+    r#type: &str,
+    domain: &Vec<&str>,
+    param: &str,
+    kind: &Kind,
+    value: &str,
+    life: u64,
+) -> Assignment {
+    Assignment {
+        var: Variable {
+            name: fruit.to_owned(),
+            value_type: SPValueType::String,
+            domain: domain
+                .iter()
+                .map(|x| String::from(x.to_owned()).to_spvalue())
+                .collect(),
+            param: Parameter::new(&param, &true),
+            r#type: r#type.to_owned(),
+            kind: kind.to_owned(),
+        },
+        val: SPValue::String(String::from(value)),
         lifetime: Duration::from_millis(life),
     }
 }
@@ -161,5 +207,326 @@ fn test_new_bool_assign_e_macro_3() {
     assert_eq!(
         true_banana,
         is_bool_fruit_true("banana", "p1", &Kind::Estimated, 1234)
+    )
+}
+
+#[test]
+fn test_new_enum_c_macro_1() {
+    let banana = enum_c!("banana", "fruit", vec!("green", "ripe", "spoiled"));
+    assert_eq!(
+        banana,
+        is_enum_fruit(
+            "banana",
+            "fruit",
+            &vec!("green", "ripe", "spoiled"),
+            "NONE",
+            &Kind::Command
+        )
+    )
+}
+
+#[test]
+fn test_new_enum_c_macro_2() {
+    let banana = enum_c!("banana", "fruit", vec!("green", "ripe", "spoiled"), "p1");
+    assert_eq!(
+        banana,
+        is_enum_fruit(
+            "banana",
+            "fruit",
+            &vec!("green", "ripe", "spoiled"),
+            "p1",
+            &Kind::Command
+        )
+    )
+}
+
+#[test]
+fn test_new_enum_m_macro_1() {
+    let banana = enum_m!("banana", "fruit", vec!("green", "ripe", "spoiled"));
+    assert_eq!(
+        banana,
+        is_enum_fruit(
+            "banana",
+            "fruit",
+            &vec!("green", "ripe", "spoiled"),
+            "NONE",
+            &Kind::Measured
+        )
+    )
+}
+
+#[test]
+fn test_new_enum_m_macro_2() {
+    let banana = enum_m!("banana", "fruit", vec!("green", "ripe", "spoiled"), "p1");
+    assert_eq!(
+        banana,
+        is_enum_fruit(
+            "banana",
+            "fruit",
+            &vec!("green", "ripe", "spoiled"),
+            "p1",
+            &Kind::Measured
+        )
+    )
+}
+
+#[test]
+fn test_new_enum_e_macro_1() {
+    let banana = enum_e!("banana", "fruit", vec!("green", "ripe", "spoiled"));
+    assert_eq!(
+        banana,
+        is_enum_fruit(
+            "banana",
+            "fruit",
+            &vec!("green", "ripe", "spoiled"),
+            "NONE",
+            &Kind::Estimated
+        )
+    )
+}
+
+#[test]
+fn test_new_enum_e_macro_2() {
+    let banana = enum_e!("banana", "fruit", vec!("green", "ripe", "spoiled"), "p1");
+    assert_eq!(
+        banana,
+        is_enum_fruit(
+            "banana",
+            "fruit",
+            &vec!("green", "ripe", "spoiled"),
+            "p1",
+            &Kind::Estimated
+        )
+    )
+}
+
+#[test]
+fn test_enum_assign_macro_1() {
+    let banana = enum_c!("banana", "fruit", vec!("green", "ripe", "spoiled"));
+    let ripe_banana = enum_assign!(banana, "ripe");
+    assert_eq!(
+        ripe_banana,
+        is_enum_fruit_something(
+            "banana",
+            "fruit",
+            &vec!("green", "ripe", "spoiled"),
+            "NONE",
+            &Kind::Command,
+            "ripe",
+            6000
+        )
+    )
+}
+
+#[test]
+fn test_enum_assign_macro_2() {
+    let banana = enum_c!("banana", "fruit", vec!("green", "ripe", "spoiled"));
+    let ripe_banana = enum_assign!(banana, "ripe", Duration::from_millis(1234));
+    assert_eq!(
+        ripe_banana,
+        is_enum_fruit_something(
+            "banana",
+            "fruit",
+            &vec!("green", "ripe", "spoiled"),
+            "NONE",
+            &Kind::Command,
+            "ripe",
+            1234
+        )
+    )
+}
+
+#[test]
+fn test_new_enum_assign_c_macro_1() {
+    let true_banana =
+        new_enum_assign_c!("banana", "fruit", vec!("green", "ripe", "spoiled"), "ripe");
+    assert_eq!(
+        true_banana,
+        is_enum_fruit_something(
+            "banana",
+            "fruit",
+            &vec!("green", "ripe", "spoiled"),
+            "NONE",
+            &Kind::Command,
+            "ripe",
+            6000
+        )
+    )
+}
+
+#[test]
+fn test_new_enum_assign_c_macro_2() {
+    let true_banana = new_enum_assign_c!(
+        "banana",
+        "fruit",
+        vec!("green", "ripe", "spoiled"),
+        "ripe",
+        "p1"
+    );
+    assert_eq!(
+        true_banana,
+        is_enum_fruit_something(
+            "banana",
+            "fruit",
+            &vec!("green", "ripe", "spoiled"),
+            "p1",
+            &Kind::Command,
+            "ripe",
+            6000
+        )
+    )
+}
+
+#[test]
+fn test_new_enum_assign_c_macro_3() {
+    let true_banana = new_enum_assign_c!(
+        "banana",
+        "fruit",
+        vec!("green", "ripe", "spoiled"),
+        "ripe",
+        "p1",
+        Duration::from_millis(1234)
+    );
+    assert_eq!(
+        true_banana,
+        is_enum_fruit_something(
+            "banana",
+            "fruit",
+            &vec!("green", "ripe", "spoiled"),
+            "p1",
+            &Kind::Command,
+            "ripe",
+            1234
+        )
+    )
+}
+
+#[test]
+fn test_new_enum_assign_m_macro_1() {
+    let true_banana =
+        new_enum_assign_m!("banana", "fruit", vec!("green", "ripe", "spoiled"), "ripe");
+    assert_eq!(
+        true_banana,
+        is_enum_fruit_something(
+            "banana",
+            "fruit",
+            &vec!("green", "ripe", "spoiled"),
+            "NONE",
+            &Kind::Measured,
+            "ripe",
+            6000
+        )
+    )
+}
+
+#[test]
+fn test_new_enum_assign_m_macro_2() {
+    let true_banana = new_enum_assign_m!(
+        "banana",
+        "fruit",
+        vec!("green", "ripe", "spoiled"),
+        "ripe",
+        "p1"
+    );
+    assert_eq!(
+        true_banana,
+        is_enum_fruit_something(
+            "banana",
+            "fruit",
+            &vec!("green", "ripe", "spoiled"),
+            "p1",
+            &Kind::Measured,
+            "ripe",
+            6000
+        )
+    )
+}
+
+#[test]
+fn test_new_enum_assign_m_macro_3() {
+    let true_banana = new_enum_assign_m!(
+        "banana",
+        "fruit",
+        vec!("green", "ripe", "spoiled"),
+        "ripe",
+        "p1",
+        Duration::from_millis(1234)
+    );
+    assert_eq!(
+        true_banana,
+        is_enum_fruit_something(
+            "banana",
+            "fruit",
+            &vec!("green", "ripe", "spoiled"),
+            "p1",
+            &Kind::Measured,
+            "ripe",
+            1234
+        )
+    )
+}
+
+#[test]
+fn test_new_enum_assign_e_macro_1() {
+    let true_banana =
+        new_enum_assign_e!("banana", "fruit", vec!("green", "ripe", "spoiled"), "ripe");
+    assert_eq!(
+        true_banana,
+        is_enum_fruit_something(
+            "banana",
+            "fruit",
+            &vec!("green", "ripe", "spoiled"),
+            "NONE",
+            &Kind::Estimated,
+            "ripe",
+            6000
+        )
+    )
+}
+
+#[test]
+fn test_new_enum_assign_e_macro_2() {
+    let true_banana = new_enum_assign_e!(
+        "banana",
+        "fruit",
+        vec!("green", "ripe", "spoiled"),
+        "ripe",
+        "p1"
+    );
+    assert_eq!(
+        true_banana,
+        is_enum_fruit_something(
+            "banana",
+            "fruit",
+            &vec!("green", "ripe", "spoiled"),
+            "p1",
+            &Kind::Estimated,
+            "ripe",
+            6000
+        )
+    )
+}
+
+#[test]
+fn test_new_enum_assign_e_macro_3() {
+    let true_banana = new_enum_assign_e!(
+        "banana",
+        "fruit",
+        vec!("green", "ripe", "spoiled"),
+        "ripe",
+        "p1",
+        Duration::from_millis(1234)
+    );
+    assert_eq!(
+        true_banana,
+        is_enum_fruit_something(
+            "banana",
+            "fruit",
+            &vec!("green", "ripe", "spoiled"),
+            "p1",
+            &Kind::Estimated,
+            "ripe",
+            1234
+        )
     )
 }
