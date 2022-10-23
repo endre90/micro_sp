4b50aa2f9c75eb58740bb3d1ad11ae8a3e94ed04
endre90 <endre90@gmail.com>
2022-10-23 18:36:16 +0200

    transition equality tested

--- src/tests/transition.rs
+++ src/tests/transition.rs
@@ -126,3 +126,93 @@ fn test_transition_ordering() {
     let s_next_1 = t1.take(&s);
     assert_eq!(s_next_1.get_spval("weight"), 90.to_spval());
 }
+
+#[test]
+#[should_panic]
+fn test_transition_ordering_panic() {
+    let s = State::new(&john_doe());
+    let weight = SPVariable::new(
+        "weight",
+        &SPValueType::Int32,
+        &vec![80.to_spval(), 85.to_spval(), 90.to_spval(), 87.to_spval()],
+    );
+    let a1 = a!(&weight, 85.cl());
+    let a2 = a!(&weight, 87.cl());
+    let a3 = a!(&weight, 90.cl());
+    let t1 = t!("gains_weight_again", eq!(&weight.cr(), 80.cl()), vec!(a1, a3, a2));
+    let s_next_1 = t1.take(&s);
+    assert_eq!(s_next_1.get_spval("weight"), 90.to_spval());
+}
+
+#[test]
+fn test_transition_equality() {
+    let weight = SPVariable::new(
+        "weight",
+        &SPValueType::Int32,
+        &vec![80.to_spval(), 85.to_spval(), 90.to_spval(), 87.to_spval()],
+    );
+    let a1 = a!(&weight, 85.cl());
+    let a2 = a!(&weight, 87.cl());
+    let a3 = a!(&weight, 90.cl());
+
+    // Transitions should be equal even if they have a different name
+    let t1 = t!("gains_weight_again", eq!(&weight.cr(), 80.cl()), vec!(a1.clone(), a2.clone(), a3.clone()));
+    let t2 = t!("gains_weight_again", eq!(&weight.cr(), 80.cl()), vec!(a1.clone(), a2.clone(), a3.clone()));
+    let t3 = t!("loses_weight_again", eq!(&weight.cr(), 80.cl()), vec!(a1.clone(), a2.clone(), a3.clone()));
+    let t4 = t!("loses_weight_again", eq!(&weight.cr(), 80.cl()), vec!(a3.clone(), a2.clone()));
+    let t5 = t!("loses_weight_again", eq!(&weight.cr(), 85.cl()), vec!(a3.clone(), a2.clone()));
+    assert_eq!(t1, t2);
+    assert_eq!(t1, t3);
+    assert_ne!(t3, t4);
+    assert_ne!(t4, t5);
+}
+
+#[test]
+fn test_transition_contained_in_vec() {
+    let weight = SPVariable::new(
+        "weight",
+        &SPValueType::Int32,
+        &vec![80.to_spval(), 85.to_spval(), 90.to_spval(), 87.to_spval()],
+    );
+    let a1 = a!(&weight, 85.cl());
+    let a2 = a!(&weight, 87.cl());
+    let a3 = a!(&weight, 90.cl());
+
+    // Transitions should be equal even if they have a different name
+    let t1 = t!("gains_weight_again", eq!(&weight.cr(), 80.cl()), vec!(a1.clone(), a2.clone(), a3.clone()));
+    let t2 = t!("gains_weight_again", eq!(&weight.cr(), 80.cl()), vec!(a1.clone(), a2.clone(), a3.clone()));
+    let t3 = t!("loses_weight_again", eq!(&weight.cr(), 80.cl()), vec!(a1.clone(), a2.clone(), a3.clone()));
+    let t4 = t!("loses_weight_again", eq!(&weight.cr(), 80.cl()), vec!(a3.clone(), a2.clone()));
+    let t5 = t!("loses_weight_again", eq!(&weight.cr(), 85.cl()), vec!(a3.clone(), a2.clone()));
+    let trans2 = vec!(t2);
+    let trans3 = vec!(t3);
+    let trans4 = vec!(t4.clone());
+    let trans5 = vec!(t4, t5);
+    assert!(trans2.contains(&t1));
+    assert!(trans3.contains(&t1));
+    assert!(!trans4.contains(&t1));
+    assert!(!trans5.contains(&t1));
+}
+
+#[test]
+fn test_transition_vec_equality() {
+    let weight = SPVariable::new(
+        "weight",
+        &SPValueType::Int32,
+        &vec![80.to_spval(), 85.to_spval(), 90.to_spval(), 87.to_spval()],
+    );
+    let a1 = a!(&weight, 85.cl());
+    let a2 = a!(&weight, 87.cl());
+    let a3 = a!(&weight, 90.cl());
+
+    // Transitions should be equal even if they have a different name
+    let t1 = t!("gains_weight_again", eq!(&weight.cr(), 80.cl()), vec!(a1.clone(), a2.clone(), a3.clone()));
+    let t2 = t!("gains_weight_again", eq!(&weight.cr(), 80.cl()), vec!(a1.clone(), a2.clone(), a3.clone()));
+    let t3 = t!("loses_weight_again", eq!(&weight.cr(), 80.cl()), vec!(a1.clone(), a2.clone(), a3.clone()));
+    let t4 = t!("loses_weight_again", eq!(&weight.cr(), 80.cl()), vec!(a3.clone(), a2.clone()));
+    let trans1 = vec!(t1.clone(), t3.clone());
+    let trans2 = vec!(t2.clone(), t3.clone());
+    let trans3 = vec!(t2.clone(), t4.clone());
+    assert_eq!(trans1, trans2);
+    assert_ne!(trans2, trans3);
+}
