532182fc9e3632a6d92e7ab3fa9a3e0bdc9d8a0e
endre90 <endre90@gmail.com>
2023-04-01 19:02:44 +0200

    relax the model

--- src/modelling/model.rs
+++ src/modelling/model.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 
 use crate::{Operation, State, Transition};
@@ -42,7 +44,33 @@ impl Model {
             name: name.to_string(),
             state: state.clone(),
             auto_transitions,
-            operations
+            operations,
+        }
+    }
+
+    // TODO: test...
+    pub fn relax(self, vars: &Vec<String>) -> Model {
+        let r_operations = self.operations.iter().map(|op| op.clone().relax(vars)).collect();
+        let r_auto_transitions = self
+            .auto_transitions
+            .iter()
+            .map(|t| t.clone().relax(vars))
+            .collect();
+        let mut r_state = HashMap::new();
+        self.state
+            .state
+            .iter()
+            .for_each(|(k, v)| match vars.contains(&k) {
+                false => {
+                    r_state.insert(k.clone(), v.clone());
+                }
+                true => (),
+            });
+        Model {
+            name: self.name,
+            state: State { state: r_state },
+            auto_transitions: r_auto_transitions,
+            operations: r_operations,
         }
     }
 }
