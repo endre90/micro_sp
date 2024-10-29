7a046f47bd527b5be41b75d80e9c67f5962c6740
endre90 <endre90@gmail.com>
2024-10-29 16:40:24 +0100

    testing with the AtomicUsize counter

--- src/running/runner.rs
+++ src/running/runner.rs
@@ -1,5 +1,5 @@
 use crate::*;
-use std::sync::{Arc, Mutex};
+use std::sync::{atomic::{AtomicUsize, Ordering}, Arc, Mutex};
 use tokio::time::{interval, Duration};
 
 // pub async fn auto_transition_runner(
@@ -43,22 +43,34 @@ use tokio::time::{interval, Duration};
 
 pub async fn operation_runner(
     model: &Model,
-    shared_state: &Arc<Mutex<State>>,
+    shared_state: &Arc<(Mutex<State>, Vec<AtomicUsize>)>, //HashMap<String, AtomicUsize>)>,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let name = &model.name;
     let mut interval = interval(Duration::from_millis(100));
     let model = model.clone();
 
     // let mut ref_count: i64 = 0;
+    let mut last_known_local_version = 0;
 
     loop {
-        let mut state = shared_state.lock().unwrap().clone();
-        // let ref_counter = state.get_or_default_i64(
-        //     &format!("{}_operation_runner", name),
-        //     &format!("{}_runner_ref_counter", name),
-        // );
-        // if ref_counter > ref_count {
-        //     ref_count = ref_counter;
+        let mut current_local_version = shared_state.1[2].load(Ordering::SeqCst);
+
+        if current_local_version != last_known_local_version {
+            println!(
+                "operation_runner: {} - {}",
+                current_local_version, last_known_local_version
+            );
+            // state has been updated by the "gantry_interface" task
+            log::warn!(target: &&format!("{}_operation_runner", name), "state has been updated by the 'operation_runner' task");
+            last_known_local_version = current_local_version;
+
+            let mut state = shared_state.0.lock().unwrap().clone();
+            // let ref_counter = state.get_or_default_i64(
+            //     &format!("{}_operation_runner", name),
+            //     &format!("{}_runner_ref_counter", name),
+            // );
+            // if ref_counter > ref_count {
+            //     ref_count = ref_counter;
 
             let mut plan_state = state.get_or_default_string(
                 &format!("{}_operation_runner", name),
@@ -181,140 +193,154 @@ pub async fn operation_runner(
                     plan_current_step.to_spvalue(),
                 )
                 .update(&format!("{}_plan", name), plan.to_spvalue());
-                // .update(
-                //     &format!("{}_runner_ref_counter", name),
-                //     (ref_counter + 1).to_spvalue(),
-                // );
+            // .update(
+            //     &format!("{}_runner_ref_counter", name),
+            //     (ref_counter + 1).to_spvalue(),
+            // );
 
-            *shared_state.lock().unwrap() = updated_state.clone();
-        // } else {
-        //     let updated_state = state.update(
-        //         &format!("{}_runner_ref_counter", name),
-        //         (ref_counter + 1).to_spvalue(),
-        //     );
-        //     *shared_state.lock().unwrap() = updated_state.clone();
-        // }
+            *shared_state.0.lock().unwrap() = updated_state.clone();
+            // } else {
+            //     let updated_state = state.update(
+            //         &format!("{}_runner_ref_counter", name),
+            //         (ref_counter + 1).to_spvalue(),
+            //     );
+            //     *shared_state.lock().unwrap() = updated_state.clone();
+            // }
+        }
         interval.tick().await;
     }
 }
 
 pub async fn planner_ticker(
     model: &Model,
-    shared_state: &Arc<Mutex<State>>,
+    shared_state: &Arc<(Mutex<State>, Vec<AtomicUsize>)>, //HashMap<String, AtomicUsize>)>,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let name = &model.name;
     let mut interval = interval(Duration::from_millis(100));
     let model = model.clone();
 
     // let mut ref_count: i64 = 0;
+    let mut last_known_local_version = 0;
 
     loop {
-        let state = shared_state.lock().unwrap().clone();
-        // let ref_counter = state.get_or_default_i64(
-        //     &format!("{}_planner_ticker", name),
-        //     &format!("{}_planner_ref_counter", name),
-        // );
-        // if ref_counter > ref_count {
-        //     ref_count = ref_counter;
-        let mut replan_trigger = state.get_or_default_bool(
-            &format!("{}_planner_ticker", name),
-            &format!("{}_replan_trigger", name),
-        );
-        let mut replanned = state.get_or_default_bool(
-            &format!("{}_planner_ticker", name),
-            &format!("{}_replanned", name),
-        );
-        let mut plan_counter = state.get_or_default_i64(
-            &format!("{}_planner_ticker", name),
-            &format!("{}_plan_counter", name),
-        );
-        let mut replan_counter = state.get_or_default_i64(
-            &format!("{}_planner_ticker", name),
-            &format!("{}_replan_counter", name),
-        );
-        let mut plan_state = state.get_or_default_string(
-            &format!("{}_planner_ticker", name),
-            &format!("{}_plan_state", name),
-        );
-        let mut plan_current_step = state.get_or_default_i64(
-            &format!("{}_planner_ticker", name),
-            &format!("{}_plan_current_step", name),
-        );
-        let mut plan = state.get_or_default_array_of_strings(
-            &format!("{}_planner_ticker", name),
-            &format!("{}_plan", name),
-        );
+        let mut current_local_version = shared_state.1[3].load(Ordering::SeqCst);
 
-        match (replan_trigger, replanned) {
-            (true, true) => {
-                log::info!(target: &&format!("{}_planner_ticker", name), "Planner triggered and (re)planned.");
-                replan_trigger = false;
-                replanned = false;
-            }
-            (true, false) => {
-                log::info!(target: &&format!("{}_planner_ticker", name), 
+        if current_local_version != last_known_local_version {
+            println!(
+                "operation_runner: {} - {}",
+                current_local_version, last_known_local_version
+            );
+            // state has been updated by the "gantry_interface" task
+            log::warn!(target: &&format!("{}_operation_runner", name), "state has been updated by the 'operation_runner' task");
+            last_known_local_version = current_local_version;
+
+            let state = shared_state.0.lock().unwrap().clone();
+            // let ref_counter = state.get_or_default_i64(
+            //     &format!("{}_planner_ticker", name),
+            //     &format!("{}_planner_ref_counter", name),
+            // );
+            // if ref_counter > ref_count {
+            //     ref_count = ref_counter;
+            let mut replan_trigger = state.get_or_default_bool(
+                &format!("{}_planner_ticker", name),
+                &format!("{}_replan_trigger", name),
+            );
+            let mut replanned = state.get_or_default_bool(
+                &format!("{}_planner_ticker", name),
+                &format!("{}_replanned", name),
+            );
+            let mut plan_counter = state.get_or_default_i64(
+                &format!("{}_planner_ticker", name),
+                &format!("{}_plan_counter", name),
+            );
+            let mut replan_counter = state.get_or_default_i64(
+                &format!("{}_planner_ticker", name),
+                &format!("{}_replan_counter", name),
+            );
+            let mut plan_state = state.get_or_default_string(
+                &format!("{}_planner_ticker", name),
+                &format!("{}_plan_state", name),
+            );
+            let mut plan_current_step = state.get_or_default_i64(
+                &format!("{}_planner_ticker", name),
+                &format!("{}_plan_current_step", name),
+            );
+            let mut plan = state.get_or_default_array_of_strings(
+                &format!("{}_planner_ticker", name),
+                &format!("{}_plan", name),
+            );
+
+            match (replan_trigger, replanned) {
+                (true, true) => {
+                    log::info!(target: &&format!("{}_planner_ticker", name), "Planner triggered and (re)planned.");
+                    replan_trigger = false;
+                    replanned = false;
+                }
+                (true, false) => {
+                    log::info!(target: &&format!("{}_planner_ticker", name), 
             "Planner triggered, initiating (re)planning.");
-                let goal = state.extract_goal(name);
-                plan_counter = plan_counter + 1;
-                replan_counter = replan_counter + 1;
-                let state_clone = state.clone();
-                let new_plan =
-                    bfs_operation_planner(state_clone, goal, model.operations.clone(), 30);
-                if !new_plan.found {
-                    log::error!(target: &&format!("{}_planner_ticker", name), "No plan was found");
-                    plan_state = PlanState::NotFound.to_string();
-                    replan_counter = 0;
-                } else {
-                    if new_plan.length == 0 {
-                        log::info!(target: &&format!("{}_planner_ticker", name), "We are already in the goal.");
-                        plan_state = PlanState::Completed.to_string();
+                    let goal = state.extract_goal(name);
+                    plan_counter = plan_counter + 1;
+                    replan_counter = replan_counter + 1;
+                    let state_clone = state.clone();
+                    let new_plan =
+                        bfs_operation_planner(state_clone, goal, model.operations.clone(), 30);
+                    if !new_plan.found {
+                        log::error!(target: &&format!("{}_planner_ticker", name), "No plan was found");
+                        plan_state = PlanState::NotFound.to_string();
+                        replan_counter = 0;
                     } else {
-                        log::info!(target: &&format!("{}_planner_ticker", name), "A new plan was found:");
-                        log::info!(target: &&format!("{}_planner_ticker", name), "Plan: {:?}", new_plan.plan);
-                        plan = new_plan.plan;
-                        plan_state = PlanState::Initial.to_string();
-                        replanned = true;
-                        plan_current_step = 0;
+                        if new_plan.length == 0 {
+                            log::info!(target: &&format!("{}_planner_ticker", name), "We are already in the goal.");
+                            plan_state = PlanState::Completed.to_string();
+                        } else {
+                            log::info!(target: &&format!("{}_planner_ticker", name), "A new plan was found:");
+                            log::info!(target: &&format!("{}_planner_ticker", name), "Plan: {:?}", new_plan.plan);
+                            plan = new_plan.plan;
+                            plan_state = PlanState::Initial.to_string();
+                            replanned = true;
+                            plan_current_step = 0;
+                        }
                     }
                 }
-            }
-            (false, _) => {
-                log::info!(target: &&format!("{}_planner_ticker", name), 
+                (false, _) => {
+                    log::info!(target: &&format!("{}_planner_ticker", name), 
             "Planner is not triggered.");
-                replanned = false;
-            }
-        };
+                    replanned = false;
+                }
+            };
 
-        let updated_state = state
-            .update(
-                &format!("{}_replan_trigger", name),
-                replan_trigger.to_spvalue(),
-            )
-            .update(&format!("{}_replanned", name), replanned.to_spvalue())
-            .update(&format!("{}_plan_counter", name), plan_counter.to_spvalue())
-            .update(
-                &format!("{}_replan_counter", name),
-                replan_counter.to_spvalue(),
-            )
-            .update(&format!("{}_plan_state", name), plan_state.to_spvalue())
-            .update(
-                &format!("{}_plan_current_step", name),
-                plan_current_step.to_spvalue(),
-            )
-            .update(&format!("{}_plan", name), plan.to_spvalue());
+            let updated_state = state
+                .update(
+                    &format!("{}_replan_trigger", name),
+                    replan_trigger.to_spvalue(),
+                )
+                .update(&format!("{}_replanned", name), replanned.to_spvalue())
+                .update(&format!("{}_plan_counter", name), plan_counter.to_spvalue())
+                .update(
+                    &format!("{}_replan_counter", name),
+                    replan_counter.to_spvalue(),
+                )
+                .update(&format!("{}_plan_state", name), plan_state.to_spvalue())
+                .update(
+                    &format!("{}_plan_current_step", name),
+                    plan_current_step.to_spvalue(),
+                )
+                .update(&format!("{}_plan", name), plan.to_spvalue());
             // .update(
             //     &format!("{}_planner_ref_counter", name),
             //     (ref_counter + 1).to_spvalue(),
             // );
 
-        *shared_state.lock().unwrap() = updated_state.clone();
-        // } else {
-        //     let updated_state = state.update(
-        //         &format!("{}_planner_ref_counter", name),
-        //         (ref_counter + 1).to_spvalue(),
-        //     );
-        //     *shared_state.lock().unwrap() = updated_state.clone();
-        // }
+            *shared_state.0.lock().unwrap() = updated_state.clone();
+            // } else {
+            //     let updated_state = state.update(
+            //         &format!("{}_planner_ref_counter", name),
+            //         (ref_counter + 1).to_spvalue(),
+            //     );
+            //     *shared_state.lock().unwrap() = updated_state.clone();
+            // }
+        }
         interval.tick().await;
     }
 }
