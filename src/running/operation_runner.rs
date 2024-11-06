0dfa5d5f92717c0aa7a7b9c451e439432e35c67d
endre90 <endre90@gmail.com>
2024-11-06 12:52:17 +0100

    operation information log

--- src/running/operation_runner.rs
+++ src/running/operation_runner.rs
@@ -19,6 +19,11 @@ pub async fn operation_runner(
     // For nicer logging
     let mut plan_state_old = "".to_string();
     let mut operation_state_old = "".to_string();
+    let mut operation_information_old = "".to_string();
+
+    // // Initialized values that we can overwrite
+    // let mut operation_state = "".to_string();
+    // let mut operation_information = "".to_string();
 
     loop {
         let (response_tx, response_rx) = oneshot::channel();
@@ -64,6 +69,11 @@ pub async fn operation_runner(
                         &format!("{}", operation.name),
                     );
 
+                    let mut operation_information = state.get_or_default_string(
+                        &format!("{}_operation_runner", name),
+                        &format!("{}_operation_information", operation.name),
+                    );
+
                     let mut operation_retry_counter = state.get_or_default_i64(
                         &format!("{}_operation_runner", name),
                         &format!("{}_retry_counter", operation.name),
@@ -75,72 +85,45 @@ pub async fn operation_runner(
                         operation_state_old = operation_state.clone()
                     }
 
+                    if operation_information_old != operation_information {
+                        log::info!(target: &format!("{}_operation_runner", name), "Current operation '{}' info: {}.", operation.name, operation_state);
+                        operation_information_old = operation_information.clone()
+                    }
+
                     match OperationState::from_str(&operation_state) {
                         OperationState::Initial => {
-                            // log::info!(target: &&format!("{}_operation_runner", name), 
-                            //     "Current state of operation '{}': Initial.", operation.name);
                             if operation.eval_running(&state) {
-                                // log::info!(target: &&format!("{}_operation_runner", name), 
-                                //     "Starting operation: '{}'.", operation.name);
                                 new_state = operation.start_running(&new_state);
                             }
                         }
                         OperationState::Disabled => todo!(),
                         OperationState::Executing => {
-                            // log::info!(target: &&format!("{}_operation_runner", name), 
-                            // "Current state of operation '{}': Executing.", operation.name);
                             if operation.can_be_completed(&state) {
                                 new_state = operation.clone().complete_running(&new_state);
-                                log::info!(target: &&format!("{}_operation_runner", name), 
-                                    "Completing operation: '{}'.", operation.name);
+                                operation_information = "Completing operation.".to_string();
                             } else if operation.can_be_failed(&state) {
                                 new_state = operation.clone().fail_running(&new_state);
-                                log::error!(target: &&format!("{}_operation_runner", name), 
-                                        "Failing operation: '{}'.", operation.name);
+                                operation_information = "Failing operation.".to_string();
                             } else {
-                                log::info!(target: &&format!("{}_operation_runner", name), 
-                                    "Waiting for operation: '{}' to be completed.", operation.name);
+                                operation_information = "Waiting to be completed.".to_string();
                             }
                         }
                         OperationState::Completed => {
-                            // log::info!(target: &&format!("{}_runner", name), 
-                            //     "Current state of operation '{}': Completed.", operation.name);
                             operation_retry_counter = 0;
                             new_state = new_state.update(
                                 &format!("{}_retry_counter", operation.name),
                                 operation_retry_counter.to_spvalue(),
                             );
                             plan_current_step = plan_current_step + 1;
-                            // let current_model_operation = model
-                            //     .operations
-                            //     .iter()
-                            //     .find(|op| op.name == current_model_operation.name)
-                            //     .unwrap()
-                            //     .to_owned();
-
-                            //             if current_model_operation
-                            //                 .clone()
-                            //                 .can_be_reset(&shared_state_local)
-                            //             {
-                            //                 log::info!(target: &&format!("{}_runner", name),
-                            // "Reseting operation: '{}'.", current_model_operation.name);
-
-                            //                 let shared_state_local = shared_state.lock().unwrap().clone();
-                            //                 let updated_state = current_model_operation
-                            //                     .clone()
-                            //                     .reset_running(&shared_state_local);
-                            //                 *shared_state.lock().unwrap() = updated_state.clone();
-                            //             }
                         }
                         OperationState::Timedout => todo!(),
                         OperationState::Failed => {
-                            // log::error!(target: &&format!("{}_operation_runner", name), 
-                            //             "Operation: '{}' has failed.", operation.name);
-
                             if operation_retry_counter < operation.retries {
                                 operation_retry_counter = operation_retry_counter + 1;
-                                log::info!(target: &&format!("{}_operation_runner", name), 
-                                    "Retrying operation: '{}'. Retry nr. {} out of {}.", operation.name, operation_retry_counter, operation.retries);
+                                operation_information = format!(
+                                    "Retrying. Retry nr. {} out of {}.",
+                                    operation_retry_counter, operation.retries
+                                );
                                 new_state = operation.clone().retry_running(&new_state);
                                 new_state = new_state.update(
                                     &format!("{}_retry_counter", operation.name),
@@ -152,41 +135,31 @@ pub async fn operation_runner(
                                     &format!("{}_retry_counter", operation.name),
                                     operation_retry_counter.to_spvalue(),
                                 );
-                                log::error!(target: &&format!("{}_operation_runner", name), 
-                                        "Failing the plan '{:?}'.", plan);
+                                operation_information =
+                                    format!("No more retries left. Failing the plan: {:?}", plan);
                                 plan_state = PlanState::Failed.to_string();
                             }
                         }
                         OperationState::UNKNOWN => (),
                     }
+
+                    new_state = new_state.update(
+                        &format!("{}_operation_information", operation.name),
+                        operation_information.to_spvalue(),
+                    );
                 } else {
-                    log::info!(target: &&format!("{}_operation_runner", name), 
-                "Completed plan: '{}'.", name);
                     plan_state = PlanState::Completed.to_string();
                 }
             }
-            PlanState::Paused => {
-                // log::info!(target: &&format!("{}_runner", name), "Current state of plan '{}': Paused.", name)
-            }
-            PlanState::Failed => {
-                // log::error!(target: &&format!("{}_runner", name), "Current state of plan '{}': Failed.", name);
-                // if operation has retried enough times it is time to fail and scrap the complete plan
-            }
-            PlanState::NotFound => {
-                // log::info!(target: &&format!("{}_runner", name), "Current state of plan '{}': NotFound.", name)
-            }
-            PlanState::Completed => {
-                // log::warn!(target: &&format!("{}_runner", name), "Current state of plan '{}': Completed.", name)
-            }
-            PlanState::Cancelled => {
-                // log::info!(target: &&format!("{}_runner", name), "Current state of plan '{}': Cancelled.", name)
-            }
-            PlanState::UNKNOWN => {
-                // log::info!(target: &&format!("{}_runner", name), "Current state of plan '{}': UNKNOWN.", name)
-            }
+            PlanState::Paused => {}
+            PlanState::Failed => {}
+            PlanState::NotFound => {}
+            PlanState::Completed => {}
+            PlanState::Cancelled => {}
+            PlanState::UNKNOWN => {}
         }
 
-        let new_state = new_state
+        new_state = new_state
             .update(&format!("{}_plan_state", name), plan_state.to_spvalue())
             .update(
                 &format!("{}_plan_current_step", name),
