d8a8d934aecde6ae8f840ca03f041f4420c0ba45
endre90 <endre90@gmail.com>
2025-06-06 15:27:46 +0200

    start time is now integer 3

--- src/running/utils.rs
+++ src/running/utils.rs
@@ -100,7 +100,7 @@ pub fn generate_operation_state_variables(model: &Model, coverability_tracking: 
     for operation in &model.operations {
         let operation_state = v!(&&format!("{}", operation.name)); // Initial, Executing, Failed, Completed, Unknown
         let operation_information = v!(&&format!("{}_information", operation.name));
-        let operation_start_time = v!(&&format!("{}_start_time", operation.name)); // to timeout if it takes too long
+        let operation_start_time = iv!(&&format!("{}_start_time", operation.name)); // to timeout if it takes too long
         let operation_retry_counter = iv!(&&format!("{}_retry_counter", operation.name)); // without scrapping the current plan, how many times has an operation retried
         state = state.add(assign!(operation_state, "initial".to_spvalue()));
         state = state.add(assign!(operation_information, SPValue::String(StringOrUnknown::UNKNOWN)));
