2120ccb1b6b3ac186442e8ab13364195f6d62fa3
endre90 <endre90@gmail.com>
2025-06-08 19:56:55 +0200

    removed lookup from tf interface temporarily

--- src/transforms/interface.rs
+++ src/transforms/interface.rs
@@ -47,33 +47,33 @@ pub async fn tf_interface(
                     &format!("{}_tf_child", sp_id),
                 );
 
-                let mut tf_lookup_result = state.get_transform_or_default_to_default(
-                    &format!("{}_tf_interface", sp_id),
-                    &format!("{}_tf_lookup_result", sp_id),
-                );
+                // let mut tf_lookup_result = state.get_transform_or_default_to_default(
+                //     &format!("{}_tf_interface", sp_id),
+                //     &format!("{}_tf_lookup_result", sp_id),
+                // );
 
                 match command.as_str() {
-                    "lookup" => {
-                        let (response_tx, response_rx) = oneshot::channel();
-                        command_sender
-                            .send(StateManagement::LookupTransform((
-                                parent.clone(),
-                                child.clone(),
-                                response_tx,
-                            )))
-                            .await?;
-                        match response_rx.await? {
-                            Some(tf) => {
-                                tf_lookup_result = tf;
-                                request_state = ServiceRequestState::Succeeded.to_string();
-                            }
-                            None => {
-                                log::error!(target: &format!("{}_tf_interface", sp_id), 
-                                    "Failed to lookup {} to {}.", parent, child);
-                                request_state = ServiceRequestState::Failed.to_string();
-                            }
-                        }
-                    }
+                    // "lookup" => {
+                    //     let (response_tx, response_rx) = oneshot::channel();
+                    //     command_sender
+                    //         .send(StateManagement::LookupTransform((
+                    //             parent.clone(),
+                    //             child.clone(),
+                    //             response_tx,
+                    //         )))
+                    //         .await?;
+                    //     match response_rx.await? {
+                    //         Some(tf) => {
+                    //             tf_lookup_result = tf;
+                    //             request_state = ServiceRequestState::Succeeded.to_string();
+                    //         }
+                    //         None => {
+                    //             log::error!(target: &format!("{}_tf_interface", sp_id), 
+                    //                 "Failed to lookup {} to {}.", parent, child);
+                    //             request_state = ServiceRequestState::Failed.to_string();
+                    //         }
+                    //     }
+                    // }
                     "reparent" => {
                         let (response_tx, response_rx) = oneshot::channel();
                         command_sender
@@ -110,11 +110,11 @@ pub async fn tf_interface(
                     .update(
                         &format!("{}_tf_request_state", sp_id),
                         request_state.to_spvalue(),
-                    )
-                    .update(
-                        &format!("{}_tf_lookup_result", sp_id),
-                        tf_lookup_result.to_spvalue(),
                     );
+                    // .update(
+                    //     &format!("{}_tf_lookup_result", sp_id),
+                    //     tf_lookup_result.to_spvalue(),
+                    // );
 
                 let modified_state = state.get_diff_partial_state(&new_state);
                 command_sender
