2bcc2d360e75dd39f03fe0bd65500bd4dd222bd1
endre90 <endre90@gmail.com>
2025-11-08 14:05:00 +0100

    elapsed exec missing in the state, added

--- src/running/state_init.rs
+++ src/running/state_init.rs
@@ -205,12 +205,14 @@ pub fn generate_operation_state_variables(model: &Model, coverability_tracking: 
     for operation in &model.auto_operations {
         let operation_state = v!(&&format!("{}", operation.name)); // Initial, Executing, Failed, Completed, Unknown
         let operation_information = v!(&&format!("{}_information", operation.name));
-        let operation_elapsed_ms = iv!(&&format!("{}_elapsed_ms", operation.name)); // to timeout if it takes too long
+        let operation_elapsed_executing_ms = iv!(&&format!("{}_elapsed_executing_ms", operation.name)); // to timeout if it takes too long
+        let operation_elapsed_disabled_ms = iv!(&&format!("{}_elapsed_disabled_ms", operation.name));
         let operation_failure_retry_counter = iv!(&&format!("{}_failure_retry_counter", operation.name)); // without scrapping the current plan, how many times has an operation retried
         let operation_timeout_retry_counter = iv!(&&format!("{}_timeout_retry_counter", operation.name));
         state = state.add(assign!(operation_state, "initial".to_spvalue()));
         state = state.add(assign!(operation_information, SPValue::String(StringOrUnknown::UNKNOWN)));
-        state = state.add(assign!(operation_elapsed_ms, SPValue::Int64(IntOrUnknown::UNKNOWN)));
+        state = state.add(assign!(operation_elapsed_executing_ms, SPValue::Int64(IntOrUnknown::UNKNOWN)));
+        state = state.add(assign!(operation_elapsed_disabled_ms, SPValue::Int64(IntOrUnknown::UNKNOWN)));
         state = state.add(assign!(operation_failure_retry_counter, SPValue::Int64(IntOrUnknown::UNKNOWN)));
         state = state.add(assign!(operation_timeout_retry_counter, SPValue::Int64(IntOrUnknown::UNKNOWN)));
 
