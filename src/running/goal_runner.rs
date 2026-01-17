a86607c9b9a4c97771f81a9e6479c4909109fab6
endre90 <endre90@gmail.com>
2026-01-17 20:22:40 +0100

    typo fix

--- src/running/goal_runner.rs
+++ src/running/goal_runner.rs
@@ -195,7 +195,7 @@ pub async fn goal_runner(
         format!("{}_plan_state", sp_id),
         format!("{}_plan", sp_id),
         format!("{}_scheduled_goals", sp_id),
-        format!("{}_incomming_goals", sp_id),
+        format!("{}_incoming_goals", sp_id),
         format!("{}_replan_trigger", sp_id),
         format!("{}_replanned", sp_id),
         format!("{}_plan_current_step", sp_id),
